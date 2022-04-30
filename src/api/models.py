c4fffb1b54894e699f786c8fe5a7d1593c119ee9
Adrián Molina Elvira <adrimo100@gmail.com>
2022-04-30 09:40:59 +0000

    Arreglado fallo

--- src/api/models.py
+++ src/api/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def serialize(self): #Sirve para tener un método que devuelva valores de usuario que normalmente queremos consultar
+    def serialize(self): #Sirve para tener un método que devuelva valores de usuario que normalmente queremos consultar.
         return {
             "id": self.id,
             "email": self.email,
